e95fd2c481bcc63b3be939d1ef6233bd94be21be
terrence-ou <outerrencedl@gmail.com>
2024-02-22 19:55:10 -0500

    events updated

--- 01_event_emitter/app.js
+++ 01_event_emitter/app.js
@@ -4,10 +4,28 @@ class Emitter extends EventEmitter {}
 
 const myEmitter = new Emitter();
 
-// defined an event
+// defined events
 myEmitter.on("foo", () => {
-  console.log("A event occurd.");
+  console.log("A event occurred 1.");
+});
+
+myEmitter.on("foo", () => {
+  console.log("A event occurred 2.");
+});
+
+myEmitter.on("foo", (text) => {
+  console.log("A event with a parameter occurred.");
+  console.log(text);
+});
+
+// Run event once (event will be removed after the first run)
+myEmitter.once("bar", () => {
+  console.log("An event occurred bar.");
 });
 
 // Firing the event
-myEmitter.emit("foo");
+myEmitter.emit("foo", "some texts");
+myEmitter.emit("bar");
+
+//The following event will not be existed
+myEmitter.emit("bar");
