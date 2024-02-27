43b8a250829ec567ec5aba8d2d3e2ea7cb156287
terrence-ou <outerrencedl@gmail.com>
2024-02-27 10:22:59 -0500

    file handler updated

--- 03_file_system/app.js
+++ 03_file_system/app.js
@@ -4,26 +4,35 @@ const fs = require("fs/promises");
 (async () => {
   const commandFileHandler = await fs.open("./command.txt", "r"); // a numeric file handler
 
+  commandFileHandler.on("change", async () => {
+    // get the size of the file
+    const size = (await commandFileHandler.stat()).size;
+    // allocate our buffer with the size of file
+    const buff = Buffer.alloc(size);
+    // offset location at which we want to start filling our buffer
+    const offset = 0;
+    // how many bytes that we want to start reading the file from
+    const length = size;
+    // the position that we want to start reading the file from
+    const position = 0;
+
+    // we always want to read the whole content (from the begining all the way to the end)
+    const content = await commandFileHandler.read(
+      buff,
+      offset,
+      length,
+      position
+    );
+    console.log(content);
+  });
+
   const watcher = fs.watch("./command.txt");
   // async iterator
   for await (const event of watcher) {
     // detecting the file chenge
     if (event.eventType === "change") {
       console.log(event); //{eventType: "...", filename: "..."}
-
-      // create a buffer to save our file
-      const size = (await commandFileHandler.stat()).size;
-      const buff = Buffer.alloc(size);
-      const offset = 0;
-      const length = size;
-      const position = 0;
-      const content = await commandFileHandler.read(
-        buff,
-        offset,
-        length,
-        position
-      );
-      console.log(content);
+      commandFileHandler.emit("change");
     }
   }
 })();
