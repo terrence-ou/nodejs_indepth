59a6c1402257cc4368e1b2d5d47c216784c9ae5c
terrence-ou <outerrencedl@gmail.com>
2024-03-04 19:34:32 -0500

    prep file updated

--- 04_Stream/00_prep/writeMany.js
+++ 04_Stream/00_prep/writeMany.js
@@ -1,11 +1,51 @@
-const fs = require("node:fs/promises");
+const fs = require("node:fs");
 
-(async () => {
+// Execution Time: 13.233s
+async function Benchmark() {
   console.time("writeMany");
-  const fileHandler = await fs.open("test.txt", "w");
+  const fileHandler = await fs.promises.open("test.txt", "w");
 
   for (let i = 0; i < 100_0000; i++) {
-    fileHandler.write(` ${i} `);
+    await fileHandler.write(` ${i} `);
   }
   console.timeEnd("writeMany");
-})();
+}
+
+// Execution Time: 1.674s
+async function BenchmarkCallback() {
+  console.time("writeMany");
+  await fs.open("text.txt", "w", (error, fd) => {
+    for (let i = 0; i < 100_0000; i++) {
+      fs.writeSync(fd, ` ${i} `);
+    }
+    console.timeEnd("writeMany");
+  });
+}
+// BenchmarkCallback();
+
+// Execution Time: 1.607s
+async function BenchmarkBuffer() {
+  console.time("writeMany");
+  await fs.open("text.txt", "w", (error, fd) => {
+    const buff = Buffer.from(` {i} `, "utf-8");
+    for (let i = 0; i < 100_0000; i++) {
+      fs.writeSync(fd, buff);
+    }
+    console.timeEnd("writeMany");
+  });
+}
+BenchmarkBuffer();
+
+// DON'T DO IT THIS WAY; IT IS ONLY FOR BENCHMARKING PURPOSE
+// Execution Time: 153.257ms
+async function BenchmarkStream() {
+  console.time("writeMany");
+  const fileHandler = await fs.promises.open("test.txt", "w");
+  const stream = fileHandler.createWriteStream();
+  for (let i = 0; i < 100_0000; i++) {
+    const buff = Buffer.from(` {i} `, "utf-8");
+    stream.write(buff);
+  }
+  console.timeEnd("writeMany");
+}
+// BenchmarkStream();
